79a5383d78b52aa5d7d116bb1850a3c6bccf30e3
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-18 15:31:42 +0200

    inited from LD250/adventforcode

---
+++ day23/src/main.rs
@@ -0,0 +1,65 @@
+//#![feature(slice_patterns)]
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::HashMap;
+
+fn main() {
+
+    let mut content = String::new();
+
+    let lines: Vec<&str>;
+
+    match File::open("data/input.txt") {
+        Err(_) => panic!("Error reading file"),
+        Ok(mut file) => {
+            file.read_to_string(&mut content).unwrap();
+            lines = content.trim().split("\n").collect();
+        },
+    };
+
+    let mut regs = HashMap::new();
+    regs.insert("a", 1);
+    regs.insert("b", 0);
+
+    let mut i: usize = 0;
+
+    let mut out = false;
+     
+    while !out {
+        match lines.get(i) {
+            None => {
+                out=true;
+            },
+
+            Some(ref s) => {
+                let operations: Vec<&str> = s.splitn(2, ' ').collect();
+                let args: Vec<&str> = operations[1].split(", ").collect();
+                let mut plus: i32 = 1;
+                match operations[0] {
+                    "jmp" => {plus = args[0].parse().expect(args[0]);}
+                    other => {
+                        let reg_value = match regs.get(&args[0]){
+                            Some(&val) => val,
+                            _ => panic!("register not found")
+                        };
+                        match other {
+                            "hlf" => {regs.insert(&args[0], reg_value / 2);},
+                            "tpl" => {regs.insert(&args[0], reg_value * 3);},
+                            "inc" => {regs.insert(&args[0], reg_value + 1);},
+                            "jie" => {plus = if reg_value % 2 == 0 {args[1].parse().expect(args[1])} else {1};},
+                            "jio" => {
+                                if reg_value == 1 {
+                                    plus = args[1].parse().expect(args[1]);
+                                };
+                            },
+                            _ => panic!("out"),
+                        };
+                    }
+                };
+                plus = i as i32 + plus;
+                i = plus as usize;
+            },
+        }
+    };
+    println!("{:?}", regs);
+}
---
+++ day23_v2/src/main.rs
@@ -0,0 +1,85 @@
+//#![feature(slice_patterns)]
+use std::io::prelude::*;
+use std::fs::File;
+use std::collections::HashMap;
+
+fn main() {
+
+    let mut content = String::new();
+
+    match File::open("data/input.txt") {
+        Err(_) => panic!("Error reading file"),
+        Ok(mut file) => {
+            file.read_to_string(&mut content).unwrap();
+            content = content.trim().replace(",", "");
+        },
+    };
+
+    let lines: Vec<&str> = content.split("\n").collect();
+    #[derive(Debug)]
+    struct Operation {
+        reg: String,
+        instr: String,
+        value: i32,
+    };
+    
+    let mut operations: Vec<Operation> = Vec::new();
+    
+    for line in &lines {
+        let ops: Vec<&str> = line.split(' ').collect();
+        let op = match ops[0] {
+            r @ "jmp" => Operation {reg: "".to_string(),
+                                    instr: r.to_string(),
+                                    value: ops[1].parse().expect(ops[1])},
+            r @ "jio" | r @ "jie" => Operation {reg: ops[1].to_string(),
+                                                instr: r.to_string(),
+                                                value: ops[2].parse().expect(ops[2])},
+            r => Operation {reg: ops[1].to_string(),
+                            instr: r.to_string(),
+                            value: 0},
+        };
+        operations.push(op);
+    };
+
+    fn run_program(reg_a_value: i32, operations: &Vec<Operation>) {
+        let mut regs = HashMap::new();
+        regs.insert("b", 0);
+        regs.insert("a", reg_a_value);
+        let mut i: usize = 0;
+
+        while i < operations.len() {
+            match operations[i] {
+
+                Operation {ref reg, ref instr, value} => {
+                    let mut plus: i32 = 1;
+                    match instr as &str {
+                        "jmp" => plus = value,
+                        other => {
+                            let reg_value = match regs.get(&**reg){
+                                Some(&val) => val,
+                                _ => panic!("register not found")
+                            };
+                            match other {
+                                "hlf" => {regs.insert(reg, reg_value / 2);},
+                                "tpl" => {regs.insert(reg, reg_value * 3);},
+                                "inc" => {regs.insert(reg, reg_value + 1);},
+                                "jie" => if reg_value % 2 == 0 {plus = value},
+                                "jio" => {
+                                    if reg_value == 1 {
+                                        plus = value;
+                                    };
+                                },
+                                _ => panic!("out"),
+                            };
+                        }
+                    };
+                    plus = i as i32 + plus;
+                    i = plus as usize;
+                },
+            }
+        };
+        println!("{:?}", regs);
+    };
+    run_program(0, &operations);
+    run_program(1, &operations);
+}
