2438ba0dda1dc5c65620603fdebd4102f2253eab
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-21 16:59:54 +0200

    day16 update

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,5 +1,6 @@
 use std::io::prelude::*;
 use std::fs::File;
+use std::collections::HashMap;
 
 fn main() {
     let mut content = String::new();
@@ -14,13 +15,30 @@ fn main() {
 
     type Thing<'a> = [(&'a str, u8); 3];
 
-
-    let aunt_things: Vec<Thing> = content.trim().split("\n").map(|line| {
+    let aunts_things: Vec<Thing> = content.trim().split("\n").map(|line| {
         let params: Vec<&str> = line.split(" ").collect();
         [(params[2], params[3].parse().unwrap()),
          (params[4], params[5].parse().unwrap()),
          (params[6], params[7].parse().unwrap())] as Thing
     }).collect();
 
-    println!("{:?}", aunt_things);
+    let mut content = String::new();
+
+    match File::open("data/ticker_tape.txt") {
+        Err(_) => panic!("Error reading file"),
+        Ok(mut file) => {
+            file.read_to_string(&mut content).unwrap();
+            content = content.replace(":", "");
+        },
+    };
+
+
+    let mut known_things: HashMap<&str, u8> = HashMap::new();
+    content.trim().split("\n").map(|line| {
+        let params: Vec<&str> = line.split(" ").collect();
+        known_things.insert(params[0], params[1].parse().unwrap())
+    });
+
+    
+    println!("{:?}", known_things);
 }
