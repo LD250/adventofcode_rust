9538a2cf03c72f98beb9a8aeb80838ee202b3863
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-22 14:17:24 +0200

    day18 try iterator

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -23,12 +23,12 @@ fn main() {
     }
 
     impl Iterator for RectIndexes {
-        type Item = usize;
-        fn next(&self) -> Option<u32> {
-            for r in [-1, 0, 1] {
+        type Item = (usize, usize);
+        fn next(&mut self) -> Option<(usize, usize)> {
+            for r in [-1, 0, 1].iter() {
                 for c in [-1, 0, 1] {
                     if ((r != 0) || (c != 0)) && (self.x + r >= 0) && (self.y + c >= 0) && (self.x + r <= 100) && (self.y + c <= 100) {
-                        Some(self.r + r, self.y + c)
+                        Some((self.r + r, self.y + c))
                     }
                 }
             }
@@ -36,22 +36,29 @@ fn main() {
         }
     }
 
-    fn get_rect_indedes(middle_index: (usize, usize)) -> [(usize, usize)] {
+/*    fn get_rect_indedes(middle_index: (usize, usize)) -> [(usize, usize)] {
         for r in [-1, 0, 1] {
             for c in [-1, 0, 1] {
             
             }
         }
-    }
+    }*/
 
     fn new_scheme(scheme: Scheme) -> Scheme {
         let mut new_scheme: Scheme;
         for r in 0..scheme.len() {
             for c in 0..scheme.len() {
-                let ids = 
+                let on_count = RectIndexes {x: &r, y: &c}.iter().filter(|coord| scheme[coord.1][coord.2] == true).count();
+                match scheme[r][c] {
+                    true if on_count == 2 || on_count == 3 => new_scheme[r][c] = true,
+                    true => new_scheme[r][c] = false,
+                    false if on_count ==3 => new_scheme[r][c] = true,
+                    false => new_scheme[r][c] = false,
+                }
             
             }
         }
+        new_scheme
     
     }
 
