697ed24302eeafa80271ae576a2fdce554f17ea3
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-23 22:24:19 +0200

    use chain with flatmap fo ccords

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -17,38 +17,23 @@ fn main() {
         line.chars().map(|c| if c == '.' {false} else {true}).collect()
     }).collect();
 
-    struct RectIndexes {
-        x: usize,
-        y: usize,
-    }
-
-    impl Iterator for RectIndexes {
-        type Item = (usize, usize);
-        fn next(&mut self) -> Option<(usize, usize)> {
-            for r in [-1, 0, 1].iter() {
-                for c in [-1, 0, 1] {
-                    if ((r != 0) || (c != 0)) && (self.x + r >= 0) && (self.y + c >= 0) && (self.x + r <= 100) && (self.y + c <= 100) {
-                        Some((self.r + r, self.y + c))
-                    }
-                }
-            }
-            None
-        }
-    }
-
-/*    fn get_rect_indedes(middle_index: (usize, usize)) -> [(usize, usize)] {
+    /*
+    fn get_rect_indedes(middle_index: (usize, usize)) -> [(usize, usize)] {
         for r in [-1, 0, 1] {
             for c in [-1, 0, 1] {
             
             }
         }
     }*/
+    // let coords: Vec<(i8, i8)> = (-1..2).map(|x: i8| (-1..2).map(|y: i8| (x, y))).iter().flat_map(|c: (i8, i8)| c).collect();
+    let coords: Vec<(i8, i8)> = (-1..2).map(|x| (-1..2).map(|y| (x.clone(), y.clone())).collect()).flat_map(|c: Vec<(i8, i8)>| c).collect();
+
 
     fn new_scheme(scheme: Scheme) -> Scheme {
-        let mut new_scheme: Scheme;
+        let mut new_scheme: Scheme = vec![];
         for r in 0..scheme.len() {
             for c in 0..scheme.len() {
-                let on_count = RectIndexes {x: &r, y: &c}.iter().filter(|coord| scheme[coord.1][coord.2] == true).count();
+                let on_count = 1;//RectIndexes {x: r, y: c}.filter(|coord| scheme[coord.0][coord.1] == true).count();
                 match scheme[r][c] {
                     true if on_count == 2 || on_count == 3 => new_scheme[r][c] = true,
                     true => new_scheme[r][c] = false,
@@ -62,6 +47,6 @@ fn main() {
     
     }
 
-    println!("{:?}", scheme);
+    println!("{:?}", coords);
 
 }
