8112c9afb56be48b86658d6c587fcc061e2600c3
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-23 17:45:49 +0200

    useless iterator try

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -18,17 +18,19 @@ fn main() {
     }).collect();
 
     struct RectIndexes {
-        x: usize,
-        y: usize,
+        x: i8,
+        y: i8,
+        modx: i8,
+        mody: i8,
     }
 
     impl Iterator for RectIndexes {
         type Item = (usize, usize);
         fn next(&mut self) -> Option<(usize, usize)> {
             for r in [-1, 0, 1].iter() {
-                for c in [-1, 0, 1] {
-                    if ((r != 0) || (c != 0)) && (self.x + r >= 0) && (self.y + c >= 0) && (self.x + r <= 100) && (self.y + c <= 100) {
-                        Some((self.r + r, self.y + c))
+                for c in [-1, 0, 1].iter() {
+                    if ((*r != 0) || (*c != 0)) && (self.x + *r >= 0) && (self.y + *c >= 0) && (self.x + *r <= 100) && (self.y + *c <= 100) {
+                        return Some(((self.x + r) as usize, (self.y + c) as usize));
                     }
                 }
             }
@@ -45,23 +47,30 @@ fn main() {
     }*/
 
     fn new_scheme(scheme: Scheme) -> Scheme {
-        let mut new_scheme: Scheme;
+        let mut new_scheme: Scheme = vec![];
         for r in 0..scheme.len() {
             for c in 0..scheme.len() {
-                let on_count = RectIndexes {x: &r, y: &c}.iter().filter(|coord| scheme[coord.1][coord.2] == true).count();
+                println!("{:?}", c);
+                let mut i = RectIndexes {x: r as i8, y: c as i8};
+                println!("{:?}", i.next());
+                println!("{:?}", i.next());
+                println!("{:?}", i.next());
+                println!("{:?}", i.next());
+                let on_count = RectIndexes {x: r as i8, y: c as i8}.filter(|coord| scheme[coord.0][coord.1] == true).count();
                 match scheme[r][c] {
                     true if on_count == 2 || on_count == 3 => new_scheme[r][c] = true,
                     true => new_scheme[r][c] = false,
                     false if on_count ==3 => new_scheme[r][c] = true,
                     false => new_scheme[r][c] = false,
                 }
+                // println!("{:?}", new_scheme);
             
             }
         }
         new_scheme
     
-    }
+    };
 
-    println!("{:?}", scheme);
+    println!("{:?}", new_scheme(scheme));
 
 }
