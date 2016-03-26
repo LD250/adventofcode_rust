8069adcf802cadcbe6973bd24f8f5e0b235c5c4f
Denys Levchenko <denis.levtchenko@gmail.com>
2016-03-26 13:01:58 +0200

    some threads draft

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,28 +1,38 @@
 use std::io::prelude::*;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
 
     fn get_new_molecules(init_molecule: String, from: &str, to: &str) -> Vec<String> {
+        println!("{:?}", init_molecule);
         let splited_molecule: Vec<&str> = init_molecule.split(from).collect();
         let molecule_len = splited_molecule.len();
         (0..molecule_len - 1).map(|i| splited_molecule.iter()
                                                       .enumerate()
                                                       .map(|(splited_index, &splited_val)| {
-                                                            if splited_index == i {
-                                                                splited_val.to_string() + to
-                                                            } else if splited_index != molecule_len - 1 {
-                                                                splited_val.to_string() + from
-                                                            } else {
-                                                                splited_val.to_string()
-                                                            }
-                                                  }).collect::<Vec<String>>().concat())
+                                                                if splited_index == i {
+                                                                    splited_val.to_string() + to
+                                                                } else if splited_index != molecule_len - 1 {
+                                                                    splited_val.to_string() + from
+                                                                } else {
+                                                                    splited_val.to_string()
+                                                                }
+                                                            }).collect::<Vec<String>>().concat())
                          .collect()
     
     
     }
 
     let replaced_molecule = get_new_molecules("HOHOH".to_string(), "H", "OO");
+    
+    let handle = thread::spawn(|| {
+        get_new_molecules("HOHOHHHHOHHHHHHHHHHOOOOOOOOOOOHHHHHHHHHHOOOOOOOOOO".to_string(), "H", "OO")
+    });
+
+    //println!("{:?}", handle.join().unwrap());
+    thread::sleep(Duration::from_millis(50));
 
 
     println!("{:?}", replaced_molecule);
