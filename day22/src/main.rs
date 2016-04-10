c70fa4fdfb40e734509779fb1546a55e8e9d6e5c
Denys Levchenko <denis.levtchenko@gmail.com>
2016-04-10 21:58:09 +0300

    day 22 update

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -7,20 +7,30 @@ Recharge costs 229 mana. It starts an effect that lasts for 5 turns. At the star
 */
 
 fn one_spell(spell_id: &str, hp: i8, mp: u8, s_e: u8, p_e: u8, r_e: u8, m_spend: u16, b_hp: u8, b_dmg: &u8, m_spend_min: &mut u16) -> u16 {
-    let bdmg = *b_gmg;
-    if s_e > 0 {
-        let s_e = s_e - 1;
-
+    if ((spell_id == "S" && s_e > 0) || (spell_id == "P" && p_e > 0) || (spell_id == "R" && r_e > 0)) {
+        return 0;
+    };
+    let (s_e, bdmg) =  if s_e > 0 {(s_e - 1, *b_dmg - 7)} else {(0, *b_dmg)};
+    let (p_e, b_hp) =  if p_e > 0 {(p_e - 1, b_hp - 3)} else {(0, b_hp)};
+    let (r_e, mp) =  if r_e > 0 {(r_e - 1, mp + 101)} else {(0, mp)};
+    if (mp <= 0 || hp <= 0) {
+        return 0;
     }
-    let (hp, mp, s_e, p_e, r_e, now_spend, b_hp) = match spell_id {
-        "M" => {(hp, mp - 53, s_e, p_e, r_e, 53, b_hp - 4)},
-        "D" => {(hp + 2, mp - 73, s_e, p_e, r_e, 73, b_hp - 2)},
-        "S" => {(hp, mp - 113, 6, p_e, r_e, 113, b_hp)},
-        "P" => {(hp, mp - 173, s_e, 6, r_e, 173, b_hp)},
-          _ => {(hp, mp - 229, s_e, p_e, 5, 229, b_hp)},
+    if (bhp <= 0) {
+        return m_spend;
+        *m_spend_min = m_spend;
+    }
+    let (hp, mp, s_e, p_e, r_e, m_spend, b_hp) = match spell_id {
+        "M" => {(hp, mp - 53, s_e, p_e, r_e, m_spend + 53, b_hp - 4)},
+        "D" => {(hp + 2, mp - 73, s_e, p_e, r_e, m_spend + 73, b_hp - 2)},
+        "S" => {(hp, mp - 113, 6, p_e, r_e, m_spend + 113, b_hp)},
+        "P" => {(hp, mp - 173, s_e, 6, r_e, m_spend + 173, b_hp)},
+          _ => {(hp, mp - 229, s_e, p_e, 5, m_spend + 229, b_hp)},
     
     };
-    0
+    if hp - bdmg <= 0 {
+        return 0;
+    }
 }
 
 
