94e70861c50ab0becc87de35a4dc6e70aa97b08e
Denys Levchenko <denis.levtchenko@gmail.com>
2016-04-10 15:18:47 +0300

    day24 part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -4,6 +4,29 @@ use std::io::prelude::*;
 use std::fs::File;
 use itertools::Itertools;
 
+fn calculate_qe(weights: &Vec<u16>, groups_num: u16, start_from: usize) -> u64 {
+    let sum: u16 = weights.iter().fold(0, |sum, &w| sum + w);
+    let group_weight = sum / groups_num;
+    let qe: u64 = (start_from..weights.len())
+                      .map(|w| {
+                          let groups: Vec<Vec<&u16>> = weights.iter()
+                                                              .combinations_n(w)
+                                                              .filter(|w_list: &Vec<&u16>| w_list.iter().fold(0, |sum, &w| sum + w) == group_weight)
+                                                              .collect();
+                          match groups.iter()
+                                      .map(|w_list: &Vec<&u16>| w_list.iter().fold(1, |pro, &&w| pro * w as u64))
+                                      .min() {
+                              Some(m) => m,
+                              _ => 0,
+                          }
+                      })
+                      .skip_while(|&x| x == 0)
+                      .take(1)
+                      .collect::<Vec<u64>>()[0];
+    qe
+
+}
+
 fn main() {
 
     let mut content = String::new();
@@ -17,27 +40,9 @@ fn main() {
     type Scheme = Vec<u16>;
 
     let weights: Vec<u16> = content.trim().split("\n").map(|w| w.parse().unwrap()).collect();
-    let sum: u16 = weights.iter().fold(0, |sum, &w| sum + w);
-    let group_weight = sum / 3;
-    let qe: u64 = (5..weights.len())
-                      .map(|w| {
-                          let groups: Vec<Vec<&u16>> = weights.iter()
-                                                              .combinations_n(w)
-                                                              .filter(|w_list: &Vec<&u16>| w_list.iter().fold(0, |sum, &w| sum + w) == group_weight)
-                                                              .collect();
-                          match groups.len() > 0 {
-                              true => {
-                                  groups.iter()
-                                        .map(|w_list: &Vec<&u16>| w_list.iter().fold(1, |pro, &&w| pro * w as u64))
-                                        .min()
-                                        .unwrap()
-                              }
-                              false => 0,
-                          }
-                      })
-                      .skip_while(|&x| x == 0)
-                      .take(1)
-                      .collect::<Vec<u64>>()[0];
+    let qe: u64 = calculate_qe(&weights, 3, 5);
+    println!("{}", qe);
+    let qe: u64 = calculate_qe(&weights, 4, 4);
     println!("{}", qe);
 
 }
